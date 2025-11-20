d9253fe2f87dcd432b629067c53aa0cd6d06950b
Martin Holy <mholy1983@protonmail.com>
2025-11-20 22:55:15 +0100

    feat: Add `logo.jpg` and configure it for PWA icons and favicon.

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
       react(),
       VitePWA({
         registerType: 'autoUpdate',
-        includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+        includeAssets: ['favicon.ico', 'logo.jpg'],
         devOptions: {
           enabled: true,
           type: 'module'
@@ -32,19 +32,19 @@ export default defineConfig(({ mode }) => {
           orientation: 'portrait',
           icons: [
             {
-              src: 'pwa-192x192.png',
+              src: 'logo.jpg',
               sizes: '192x192',
-              type: 'image/png'
+              type: 'image/jpeg'
             },
             {
-              src: 'pwa-512x512.png',
+              src: 'logo.jpg',
               sizes: '512x512',
-              type: 'image/png'
+              type: 'image/jpeg'
             },
             {
-              src: 'pwa-512x512.png',
+              src: 'logo.jpg',
               sizes: '512x512',
-              type: 'image/png',
+              type: 'image/jpeg',
               purpose: 'any maskable'
             }
           ]
